b4300f5f2ce910448233db532cbb9f8dc33c7755
liuzheng712 <liuzheng712@gmail.com>
2017-10-14 19:06:31 +0800

    feat: update

--- golog.go
+++ golog.go
@@ -7,21 +7,20 @@ import (
 	"flag"
 )
 
-var Log = logging.MustGetLogger("example")
-
-// Example format string. Everything except the message has a custom color
-// which is dependent on the log level. Many fields have a custom output
-// formatting too, eg. the time returns the hour down to the milli second.
-var backendformat = logging.MustStringFormatter(
-	`%{time:2006-01-02 15:04:05.000} %{callpath} %{id:03x} %{message}`,
-)
-var frontendformat = logging.MustStringFormatter(
-	`%{color}%{time:2006-01-02 15:04:05.000} %{callpath} %{id:03x}%{color:reset} %{message}`,
-)
 var (
-	LogLevel = flag.String("loglevel", "INFO", "set the console log level")
-	LogPath = flag.String("logpath", "", "set the logfile path")
-	LogSelector = flag.String("logselector", "*", "Using select string to filter the log")
+	Log = logging.MustGetLogger("example")
+
+	backendformat = logging.MustStringFormatter(
+		`%{time:2006-01-02 15:04:05.000} %{level} %{message}`,
+	)
+
+	frontendformat = logging.MustStringFormatter(
+		`%{color}%{time:2006-01-02 15:04:05.000} %{level} %{message}`,
+	)
+	debug = false
+	logLevel = flag.String("loglevel", "INFO", "set the console log level")
+	logPath = flag.String("logpath", "", "set the logfile path")
+	logSelector = flag.String("logselector", "*", "Using select string to filter the log")
 )
 
 // Password is just an example type implementing the Redactor interface. Any
@@ -29,13 +28,25 @@ var (
 type Password string
 
 func Initial() {
-	Logs(*LogPath, *LogLevel, "INFO")
+	Logs(*logPath, *logLevel, "INFO")
 }
 func (p Password) Redacted() interface{} {
 	return logging.Redact(string(p))
 }
 
 func Logs(logpath, frontend, backend string) (*logging.Logger, error) {
+	if frontend == "DEBUG" {
+		frontendformat = logging.MustStringFormatter(
+			`%{color}%{time:2006-01-02 15:04:05.000} %{callpath} %{id:03x}%{color:reset} %{message}`,
+		)
+		debug = true
+	}
+	if backend == "DEBUG" {
+		backendformat = logging.MustStringFormatter(
+			`%{time:2006-01-02 15:04:05.000} %{callpath} %{id:03x} %{message}`,
+		)
+		debug = true
+	}
 	Frontend := logging.NewLogBackend(os.Stderr, "", 0)
 	FrontendFormatter := logging.NewBackendFormatter(Frontend, frontendformat)
 	FrontendLeveled := logging.AddModuleLevel(FrontendFormatter)
@@ -71,44 +82,77 @@ func Logs(logpath, frontend, backend string) (*logging.Logger, error) {
 }
 
 func Debug(selector string, format string, v ...interface{}) {
-	if *LogSelector == "*" || *LogSelector == selector {
+	if *logSelector == "*" || *logSelector == selector {
 		Log.Debugf("[%v] " + format, append([]interface{}{selector}, v...)...)
 	}
 }
+
 func Info(selector string, format string, v ...interface{}) {
-	if *LogSelector == "*" || *LogSelector == selector {
-		Log.Infof("[%v] " + format, append([]interface{}{selector}, v...)...)
+	if *logSelector == "*" || *logSelector == selector {
+		if debug {
+			Log.Infof("[%v] " + format, append([]interface{}{selector}, v...)...)
+		} else {
+			Log.Infof(format, v...)
+		}
 	}
 }
 
 func Notice(selector string, format string, v ...interface{}) {
-	if *LogSelector == "*" || *LogSelector == selector {
-		Log.Noticef("[%v] " + format, append([]interface{}{selector}, v...)...)
+	if *logSelector == "*" || *logSelector == selector {
+		if debug {
+			Log.Noticef("[%v] " + format, append([]interface{}{selector}, v...)...)
+		} else {
+			Log.Noticef(format, v...)
+		}
 	}
 }
+
 func Warn(selector string, format string, v ...interface{}) {
-	if *LogSelector == "*" || *LogSelector == selector {
-		Log.Warningf("[%v] " + format, append([]interface{}{selector}, v...)...)
+	if *logSelector == "*" || *logSelector == selector {
+		if debug {
+			Log.Warningf("[%v] " + format, append([]interface{}{selector}, v...)...)
+		} else {
+			Log.Warningf(format, v...)
+		}
 	}
 }
+
 func Error(selector string, format string, v ...interface{}) {
-	if *LogSelector == "*" || *LogSelector == selector {
-		Log.Errorf("[%v] " + format, append([]interface{}{selector}, v...)...)
+	if *logSelector == "*" || *logSelector == selector {
+		if debug {
+			Log.Errorf("[%v] " + format, append([]interface{}{selector}, v...)...)
+		} else {
+			Log.Errorf(format, v...)
+		}
 	}
 }
 
 func Critical(selector string, format string, v ...interface{}) {
-	if *LogSelector == "*" || *LogSelector == selector {
-		Log.Criticalf("[%v] " + format, append([]interface{}{selector}, v...)...)
+	if *logSelector == "*" || *logSelector == selector {
+		if debug {
+			Log.Criticalf("[%v] " + format, append([]interface{}{selector}, v...)...)
+		} else {
+			Log.Criticalf(format, v...)
+		}
 	}
 }
+
 func Panic(selector string, format string, v ...interface{}) {
-	if *LogSelector == "*" || *LogSelector == selector {
-		Log.Panicf("[%v] " + format, append([]interface{}{selector}, v...)...)
+	if *logSelector == "*" || *logSelector == selector {
+		if debug {
+			Log.Panicf("[%v] " + format, append([]interface{}{selector}, v...)...)
+		} else {
+			Log.Panicf(format, v...)
+		}
 	}
 }
+
 func Fatal(selector string, format string, v ...interface{}) {
-	if *LogSelector == "*" || *LogSelector == selector {
-		Log.Fatalf("[%v] " + format, append([]interface{}{selector}, v...)...)
+	if *logSelector == "*" || *logSelector == selector {
+		if debug {
+			Log.Fatalf("[%v] " + format, append([]interface{}{selector}, v...)...)
+		} else {
+			Log.Fatalf(format, v...)
+		}
 	}
 }
